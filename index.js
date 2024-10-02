878fdd0d152834304642b60fd42638787e445398
subhadra2002 <subhadramati0@gmail.com>
2024-10-02 23:10:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+const questions = [
+    {
+        question: "Who is Caption Cool ?",
+        answers:[
+            {text:"S.Tendulkar", correct: false},
+            {text:"V.Kohil", correct: false},
+            {text:"Ms.Dhoni", correct: true},
+            {text:"R.Sharma", correct: false},
+        ]
+    },
+    {
+        question: "What is the Jersy Number of Ms.Dhoni ?",
+        answers:[
+            {text:"7", correct: true},
+            {text:"10", correct: false},
+            {text:"8", correct: false},
+            {text:"19", correct: false},
+        ]  
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next"
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    
+    currentQuestion.answers.forEach((answer)=>{
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);  
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button=>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `you scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Start Again"
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex< questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+});
+
+
+startQuiz();
+
